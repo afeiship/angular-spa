20648e7fbe09ecceb3b60ca8ef9c0650f7e9fab9
feizheng <1290657123@qq.com>
2016-08-31 00:51:38 +0800

    update inject task

--- gulp/inject.js
+++ gulp/inject.js
@@ -11,17 +11,21 @@
   });
 
 
-  var gulpBowerStream = $.bowerStream();
-  var appStream = gulp.src(path.join(config.path.src, 'app/**/*.js'))
+  var gulpJsBowerStream = $.bowerStream({filter: '**/*.js'});
+  var gulpCssBowerStream = $.bowerStream({filter: '**/*.css'});
+  var appJsStream = gulp.src(path.join(config.path.src, 'app/**/*.js'))
     .pipe($.angularFilesort());
 
 
   gulp.task('inject-app', function() {
     return gulp.src(path.join(config.path.src, './index.html'))
-      .pipe($.inject(
-        $.streamSeries(gulpBowerStream, appStream), {
-          name: 'bower-app'
-        }))
+      .pipe(
+        $.inject(
+          $.streamSeries(gulpJsBowerStream, appJsStream,gulpCssBowerStream), {
+            name: 'bower-app'
+          }
+        )
+      )
       .pipe(gulp.dest('dist'));
   });
 
