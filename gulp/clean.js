e7621d1631d451d7e579f440beb5cd79fd657ac4
feizheng <1290657123@qq.com>
2016-09-09 23:03:23 +0800

    bugfix

--- gulp/clean.js
+++ gulp/clean.js
@@ -14,14 +14,18 @@
 
   //clean
   gulp.task('clean', function() {
-    return $.del(config.paths.dist);
+    return $.del([
+      config.paths.dist,
+      path.join(config.paths.src,'partial')
+    ]);
   });
 
 
   gulp.task('clean:app', function() {
-    return $.del(
-      path.join(config.paths.dist, 'app')
-    );
+    return $.del([
+      path.join(config.paths.dist, 'app'),
+      path.join(config.paths.src,'partials')
+    ]);
   });
 
 
